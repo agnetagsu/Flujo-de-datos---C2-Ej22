a753409d95bc8fc953d85fa56e75288dda4587c4
Agneta <agnetagsu@yahoo.es>
2021-05-22 21:56:50 +0200

    Agregar archivos de proyecto.

---
+++ Flujo de datos - C2 Ej22/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Flujo_de_datos___C2_Ej22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String password = "notenter00";
+            String passwordintroducida;
+            int intentos = 0;
+            Boolean acierto = false;
+
+            do
+            {
+                Console.WriteLine("Introduzca la contraseña:");
+                passwordintroducida = Console.ReadLine();
+                intentos++;
+
+                if (password == passwordintroducida)
+                {
+                    acierto = true;
+                }
+            }
+            while ((intentos < 3) & (acierto == false));
+
+            if(acierto)
+            {
+                Console.WriteLine("Enhorabuena");
+            }
+            else
+            {
+                Console.WriteLine("Se excedió el número de intentos");
+            }
+            
+        }
+    }
+}
